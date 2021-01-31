f3dfbd5a2f0febca38d4300df54df1a472d52e30
Leandro Henrique <emtudo@gmail.com>
2021-01-31 06:59:26 -0300

    added php 8

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 php:
 	- cd base-php && docker build -t ciareis/base-php:latest . --network=host
 	- docker push ciareis/base-php:latest
+	- cd base-php && docker build -t ciareis/base-php:php-8.0 . --network=host
+	- docker push ciareis/base-php:php-8.0
+
+php74:
 	- cd base-php && docker build -t ciareis/base-php:php-7.4 . --network=host
 	- docker push ciareis/base-php:php-7.4
+
 build:
 	- cd base-php && docker build -t ciareis/base-php:latest . --network=host
+	- cd base-php && docker build -t ciareis/base-php:php-8.0 . --network=host
+
+build74:
 	- cd base-php && docker build -t ciareis/base-php:php-7.4 . --network=host
--- base-php/start.sh
+++ base-php/start.sh
@@ -8,42 +8,42 @@ sudo chown -R app:app /var/www/app
 cp /home/bashrc /home/app/.bashrc
 
 # Set PHP memory limit value.
-sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php7/php.ini
-
-sudo ln -s /usr/lib/php7/modules/intl.so /usr/lib/php7/modules/intl.so.so
+sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php8/php.ini
 
 # OPCache extreme mode.
 if [[ $OPCACHE_MODE == "extreme" ]]; then
   # enable extreme caching for OPCache.
-  echo "opcache.enable=1" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.memory_consumption=512" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.interned_strings_buffer=128" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.max_accelerated_files=32531" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.validate_timestamps=0" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.save_comments=1" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
-  echo "opcache.fast_shutdown=0" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.enable=1" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.memory_consumption=512" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.interned_strings_buffer=128" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.max_accelerated_files=32531" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.validate_timestamps=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.save_comments=1" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.fast_shutdown=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
 fi
 
 # OPCache disabled mode.
 if [[ $OPCACHE_MODE == "disabled" ]]; then
   # disable extension.
-  sudo sed -i "/zend_extension=opcache/c\;zend_extension=opcache" /etc/php7/conf.d/00_opcache.ini
+  sudo sed -i "/zend_extension=opcache/c\;zend_extension=opcache" /etc/php8/conf.d/00_opcache.ini
   # set enabled as zero, case extension still gets loaded (by other extension).
-  echo "opcache.enable=0" | sudo tee -a /etc/php7/conf.d/00_opcache.ini > /dev/null
+  echo "opcache.enable=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
 fi
 
 if [[ $XDEBUG_ENABLED == true ]]; then
-   # enable xdebug extension
-   sudo sed -i "/;zend_extension=xdebug/c\zend_extension=xdebug" /etc/php7/conf.d/00_xdebug.ini
-   # enable xdebug remote config
-   echo "[xdebug]" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.remote_enable=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.remote_port=9000" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.scream=0" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.cli_color=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-   echo 'xdebug.idekey = "app"' | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    # enable xdebug extension
+    sudo sed -i "/;zend_extension=xdebug/c\zend_extension=xdebug" /etc/php8/conf.d/00_xdebug.ini
+
+    # enable xdebug remote config
+    echo "[xdebug]" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_enable=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    # shellcheck disable=SC2006
+    echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_port=9000" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.scream=0" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.cli_color=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo 'xdebug.idekey = "app"' | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
 fi
 
 if [[ $LARAVEL_CACHE == true ]]; then
@@ -63,7 +63,7 @@ if [[ $SUPERVISOR == true ]]; then
   sudo chown -R app:app /var/run
   sudo chown -R app:app /run
   echo -e "\n # ---> Crontab \n" && \
-  (echo '* * * * * /usr/bin/php7 /var/www/app/artisan schedule:run') | crontab -
+  (echo '* * * * * /usr/bin/php8 /var/www/app/artisan schedule:run') | crontab -
   /usr/sbin/crond
   if [[ $NGINX_ENABLED == false ]]; then
     /usr/bin/supervisord -n -c /etc/supervisord.conf
@@ -76,7 +76,7 @@ if [[ $NGINX_ENABLED == true ]]; then
   sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
   # Starts FPM
-  nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
+  nohup /usr/sbin/php-fpm8 -y /etc/php8/php-fpm.conf -F -O 2>&1 &
 
   # Starts NGINX!
   nginx
